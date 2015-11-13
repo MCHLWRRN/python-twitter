48b35e409ff389fbe511b026a90a0c01760722b8
Michael Warren <me@mchlwrrn.com>
2015-11-12 19:45:39 -0800

    first python commit

---
+++ config.py
@@ -0,0 +1,7 @@
+#Please put the values here.
+key="Tsw5WTrjv6WJj6CYGeJqqC0l9"
+secret="jCBwjFXnWTgHGEg8OHeNMtbwIGvwUYqixulb35hlXqG4lD0HGE"
+access_key="3315444565-xQqEsrwhZadRYgVn6HaD478EiJk5KEWs7xmdUFj"
+access_secret="6XUPwiMkTbZNOBck8lWmseFhdnsQoRhVyBdeDWWWunWk7"
+
+
